a014858d1f53912af57bc1412e0862501d56bd7b
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2021-03-17 16:53:51 -0700

    first app

---
+++ annotations/arrays.ts
@@ -0,0 +1,10 @@
+// const carMarker: string[] = [];
+const carMakers = ["ford", "toyota", "chevy"];
+const dates = [new Date(), new Date()];
+
+// const carByMake: string[][] = [];
+const carByMake = [["f150"], ["corolla"], ["camaro"]];
+
+// Help with inference when extracting values
+const cars = carMakers[0];
+const myCar = carMakers.pop();
---
+++ tupples.ts
@@ -0,0 +1,16 @@
+const drink = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+};
+type Drink = [string, boolean, number];
+const pepsi: Drink = ["brown", true, 40];
+const sprite: Drink = ["clear", true, 60];
+const tea: Drink = ["black", false, 10];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
